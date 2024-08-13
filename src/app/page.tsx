6d3b050ba6012e00ee8cfd93b04b9fb56d0ac936
prateek singh <ps0431299@gmail.com>
2024-08-14 03:12:16 +0530

    hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>protfolio</h1>
+    <main className=" mt-20 mx-auto w-[65%]">
+      <div className="grid grid-cols-2 gap-4 ">
+        <div className="flex flex-col items-start justify-center gap-4">
+          <p className=" text-white text-lg font-semibold">
+            Prateek is a <span className="text-Primary">Software Engineer</span>{" "}
+            and a <span className="text-Primary">Full Stack Developer.</span>
+          </p>
+          <p className="text-Secondary text-xs font-medium mt-2">
+            He crafts responsive websites where technologies meet creativity
+          </p>
+
+          <button className="border-[1px] border-Primary text-white px-2 py-1  hover:bg-[#483b55] duration-500 text-sm font-normal">
+            {" "}
+            Contact me!!
+          </button>
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/Style=Outline.svg"
+            alt="hero-left-img"
+            width={400}
+            height={400}
+            className="  absolute top-8 left-0 w-40 -z-10"
+          />
+          <Image src="/Image.svg" alt="Prateek" width={400} height={400} />
+          <Image
+            src="/Dots.svg"
+            alt="Prateek"
+            width={400}
+            height={400}
+            className="  absolute bottom-14 right-4 w-16 z-10"
+          />
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,28 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="border-l-2 border-Secondary  "> </div>
-      <div className=" flex justify-between items-center w-[60%] mx-auto mt-4">
+      <div className="h-[10rem] w-[2px] bg-Secondary fixed top-0 left-10 ">
+        {" "}
+      </div>
+      <div>
+        <img
+          src="/Github.svg"
+          alt=""
+          className=" fixed  left-[1.7rem] top-[10rem] "
+        />
+        <img
+          src="/Discord.svg"
+          alt=""
+          className=" fixed  left-[1.7rem] top-[12rem] "
+        />
+        <img
+          src="/Linkedin.svg"
+          alt=""
+          className=" fixed  left-[1.7rem] top-[14rem] "
+        />
+      </div>
+
+      <div className=" flex justify-between items-center w-[65%] mx-auto mt-4">
         <div className="flex items-center justify-center gap-2">
           <img src="/Union.svg" alt="" className="w-4" />
           <h1 className="text-white text-sm">Prateek</h1>
